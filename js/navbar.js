674c7c79a40590a4997f2bc2e37f5cb65ed11268
MuhammadRagil <mragill98@gmail.com>
2021-06-02 22:47:29 +0700

    Initial Commit

---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-5000px";
+  }
+  prevScrollpos = currentScrollPos;
+};
---
+++ js/typingAnimation.js
@@ -0,0 +1,34 @@
+var typeText = document.querySelector("#type-text");
+var textToBeTyped = "I'm a fullstack web developer.";
+var index = 0,
+  isAdding = true;
+
+function playAnim() {
+  setTimeout(function () {
+    typeText.innerText = textToBeTyped.slice(0, index);
+    if (isAdding) {
+      if (index > textToBeTyped.length) {
+        isAdding = false;
+        setTimeout(function () {
+          playAnim();
+        }, 3000);
+        return;
+      } else {
+        // increment index by 1
+        index++;
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        isAdding = true;
+      } else {
+        index--;
+      }
+    }
+    // call itself
+    playAnim();
+  }, 75);
+}
+// start animation
+// playAnim();
+setTimeout(playAnim, 1000);
